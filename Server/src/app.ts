00b2b953e11209d45390d1f38a87b309523a1157
Abdelrahman Khalaf <abdlrhmankhalaf566@gmail.com>
2025-05-01 18:44:22 +0300

    Add configuration of prisma

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import bcrpt from 'bcrypt';
+import { Request,Response,NextFunction } from 'express';
+import { PrismaClient } from '../generated/prisma';
+
+const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
