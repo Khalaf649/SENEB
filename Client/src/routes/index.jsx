adf13c61ce972c570ba1afd744cf2ced2bc8eb2f
Amany Othman <165468609+Amany-othman@users.noreply.github.com>
2025-07-19 23:35:43 +0300

    Blood Stock pade Done

--- Client/src/routes/index.jsx
+++ Client/src/routes/index.jsx
@@ -11,11 +11,11 @@ import HealthFacility from "../pages/Dashboard/HealthFacility/HealthFacility";
 import ManageSubadmins from "../pages/Dashboard/Admin/ManageSubadmins";
 import ManageBloodCenters from "../pages/Dashboard/Admin/ManageBloodCenters";
 import ViewDonations from "../pages/Dashboard/Admin/ViewDonations";
+import BloodStock from "../pages/Dashboard/Admin/BloodStock";
 
 import CenterData from "../pages/Dashboard/SubAdmin/CenterData";
 import Appointments from "../pages/Dashboard/SubAdmin/Appointments";
 import ApproveRequests from "../pages/Dashboard/SubAdmin/ApproveRequests";
-
 
 export default function AppRoutes() {
   return (
@@ -31,7 +31,8 @@ export default function AppRoutes() {
           <Route index element={<MainPage />} />
           <Route path="manage-subadmins" element={<ManageSubadmins />} />
           <Route path="manage-blood-centers" element={<ManageBloodCenters />} />
-          <Route path="view-donations" element={<ViewDonations/>}/>
+          <Route path="view-donations" element={<ViewDonations />} />
+          <Route path="center-data" element={<BloodStock />} />
         </Route>
 
         {/* Sub Admin */}
